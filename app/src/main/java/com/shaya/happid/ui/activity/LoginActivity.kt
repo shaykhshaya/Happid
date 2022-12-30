f4cbf9c3b393c89a7e336b96add1e408a0f00009
Mohd Shaya <shaykhshaya>
2022-12-30 23:37:54 +0530

    ui fixes

--- app/src/main/java/com/shaya/happid/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/shaya/happid/ui/activity/LoginActivity.kt
@@ -8,17 +8,17 @@ import com.shaya.happid.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    lateinit var mobNum:String
+    lateinit var mobNum: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imgBack.setOnClickListener { onBackPressed() }
 
 
         binding.requestOtp.setOnClickListener {
-
 
 
             mobNum = binding.inputNumber.text.toString()
@@ -26,25 +26,22 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-            OtpDialogFragment(genOTP(mobNum), mobNum).show(supportFragmentManager, OtpDialogFragment::class.java.name)
-
-
-
-
+            OtpDialogFragment(genOTP(mobNum), mobNum).show(
+                supportFragmentManager,
+                OtpDialogFragment::class.java.name
+            )
 
 
         }
     }
 
-    private fun genOTP(mobNum:String): String{
-      //  val num = mobNum.toList()
-        val num = mobNum.substring(0,2) + mobNum.substring(8,10)
-       // val otp = (listOf(num[0],num[1],num[8],num[9])).toString()
+    private fun genOTP(mobNum: String): String {
+        //  val num = mobNum.toList()
+        val num = mobNum.substring(0, 2) + mobNum.substring(8, 10)
+        // val otp = (listOf(num[0],num[1],num[8],num[9])).toString()
         return num
     }
 
 
-
-
-    }
+}
 
